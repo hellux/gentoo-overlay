48faecdd9e7d78fe948cff66063f0be8dde3efcd
Noah Hellman <noah.hellman@protonmail.com>
2020-11-15 20:12:27 +0100

    check-releases script: exit on errors

--- meta/check-releases.sh
+++ meta/check-releases.sh
@@ -22,6 +22,8 @@ then packages=$1
 else packages=upstream-sources
 fi
 
+[ -r "$packages" ] || exit 1
+
 PKGS=$(mktemp)
 VERSIONS=$(mktemp)
 HTMLS=$(mktemp -d)
@@ -33,7 +35,8 @@ sed -z 's/\n[\t ]/ /g' "$packages" | tr -s " \n" > "$PKGS"
 # Query installed overlays and the portage tree for ebuild versions for each
 # package.
 atoms=$(cut -d" " -f1 "$PKGS")
-equery l -opF '$category/$name	$version' $atoms | tr -s '\n' > "$VERSIONS"
+equery l -opF '$category/$name	$version' $atoms | tr -s '\n' > "$VERSIONS" \
+    || exit 2
 
 # Fetch the download page for each package.
 curl_args=""
@@ -41,21 +44,25 @@ while read -r atom _ url; do
     mkdir -p "$HTMLS/$(dirname "$atom")"
     curl_args="$curl_args -o $HTMLS/$atom $url "
 done < "$PKGS"
-curl -LsZ $curl_args
+curl -LsZ $curl_args || exit 3
 
 # Compare latest upstream package with latest ebuild.
 while read -r atom regex _; do
+    printf "%s" "$atom: "
+
     ere=$(echo "$regex" | tr -d "@")
     upstream_package=$(grep -oE "$ere" "$HTMLS/$atom" | sort -rV | head -n1)
+    [ -z "$upstream_package" ] && exit 4
 
     prefix=$(echo "$regex" | cut -d@ -f1 | tr -d '\\')
     suffix=$(echo "$regex" | cut -d@ -f3 | tr -d '\\')
     ebuild_version=$(grep "$atom" "$VERSIONS" | cut -f2 | sort -rV | head -n1)
     ebuild_package=$prefix$ebuild_version$suffix
+    [ -z "$ebuild_version" ] && exit 5
 
     if [ "$ebuild_package" = "$upstream_package" ];
-    then echo "$atom: up to date -- $ebuild_version"
-    else echo "$atom: NEW VERSION available -- $upstream_package"
+    then echo "up to date -- $ebuild_version"
+    else echo "NEW VERSION available -- $upstream_package"
     fi
 done < "$PKGS"
 
